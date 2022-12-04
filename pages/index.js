2cfecda69a926a0356797824c4b03ba69a28cecb
HydratedPeridot <hydratedp@gmail.com>
2022-12-04 21:14:01 +0100

    hello world!

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      minWidth: '100vw',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      Hello world
+    </div>
+  )
+}
+
+export default Home
